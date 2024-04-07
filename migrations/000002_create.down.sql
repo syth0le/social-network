85271d8c57197ec3c2df7c03f9ab76a56f81b7da
syth0le <chdan565@gmail.com>
2024-04-07 21:30:56 +0300

    added migrations

--- migrations/000002_create.down.sql
+++ migrations/000002_create.down.sql
@@ -1,2 +1,5 @@
 DROP INDEX user_first_name_idx;
 DROP INDEX user_second_name_idx;
+
+DROP EXTENSION pg_trgm;
+DROP EXTENSION btree_gin;
--- migrations/000002_create.up.sql
+++ migrations/000002_create.up.sql
@@ -1,4 +1,5 @@
 -- CREATE INDEX user_full_name_idx ON user_table (first_name, second_name);
-CREATE EXTENSION pg_trgm;
-CREATE INDEX user_first_name_idx ON user_table USING gin (first_name, gin_trgm_ops);
-CREATE INDEX user_second_name_idx ON user_table USING gin (second_name, gin_trgm_ops);
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+CREATE EXTENSION IF NOT EXISTS btree_gin;
+CREATE INDEX user_first_name_idx ON user_table USING gin (first_name gin_trgm_ops);
+CREATE INDEX user_second_name_idx ON user_table USING gin (second_name gin_trgm_ops);
