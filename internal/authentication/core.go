f70564580ea7dab92da5130101e96e28c94688d4
syth0le <chdan565@gmail.com>
2024-06-17 00:21:05 +0300

    added error writer in interceptor

--- internal/authentication/core.go
+++ internal/authentication/core.go
@@ -2,9 +2,12 @@ package authentication
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 
+	"github.com/go-http-utils/headers"
+	xerrors "github.com/syth0le/gopnik/errors"
 	"go.uber.org/zap"
 
 	"github.com/syth0le/social-network/internal/service/user"
@@ -26,7 +29,7 @@ func (s *Service) AuthenticationInterceptor(next http.Handler) http.Handler {
 
 		userID, err := s.TokenManager.ValidateToken(authToken)
 		if err != nil {
-			s.Logger.Sugar().Errorf("validate token: %v", err)
+			s.writeError(w, fmt.Errorf("validate token: %w", err))
 			return
 		}
 
@@ -45,4 +48,25 @@ func (s *Service) ValidateToken(authToken string) error {
 	}
 
 	return nil
+}
+
+func (s *Service) writeError(w http.ResponseWriter, err error) {
+	s.Logger.Sugar().Warnf("http response error: %v", err)
+
+	w.Header().Set(headers.ContentType, "application/json")
+	errorResult, ok := xerrors.FromError(err)
+	if !ok {
+		s.Logger.Sugar().Errorf("cannot write log message: %v", err)
+		return
+	}
+	w.WriteHeader(errorResult.StatusCode)
+	err = json.NewEncoder(w).Encode(
+		map[string]any{
+			"message": errorResult.Msg,
+			"code":    errorResult.StatusCode,
+		})
+
+	if err != nil {
+		http.Error(w, xerrors.InternalErrorMessage, http.StatusInternalServerError) // TODO: make error mapping
+	}
 }
